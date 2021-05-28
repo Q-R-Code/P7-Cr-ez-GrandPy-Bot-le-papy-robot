f23a84c6a9bdd47f57b0596a36299ac0f1850a77
Quentin RUFFIER <qruffier@gmail.com>
2021-05-28 16:21:55 +0200

    MAJ Template, add logo.

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # To generate a new secret key:
 # >>> import random, string
 # >>> "".join([random.choice(string.printable) for _ in range(24)])
-SECRET_KEY = '\\k<;;\t:VMi2&(eC}`\\d`3vY['
+SECRET_KEY = "X.\\Y\\ncCj%w`7Z9TK).jq[S<"
